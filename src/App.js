716ddeb9a246963cb32fcd5d0358bc282bfd0e70
renekrewinkel <rene@studiokrijst.com>
2023-05-26 13:46:21 +0200

    Aanpassing MET hook en filter op zowel username als email

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import {useEffect, useState} from 'react'
 import config from './config/config'
-// import useFetch from './hooks/useFetch'
+import useFetch from './hooks/useFetch'
 import SearchBar from './components/SearchBar'
 import List from './components/List'
 import AppContext from './context/AppContext'
 
 const App = () => {
 
-  const [data, setData] = useState([])
-  const [isLoaded, setLoaded] = useState(false)
+  const [data, isLoaded] = useFetch(config.USER_ENDPOINT)
   const [filtered, setFiltered] = useState([])
   const [value, setValue] = useState('')
 
-  useEffect(() => {
-    fetch(config.USER_ENDPOINT)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setFiltered(data)
-        setLoaded(true)
-      })
-  }, [])
+  useEffect( () => {
+    if(isLoaded) {
+      setFiltered(data)
+    }
+  }, [isLoaded])
+
 
   const clearSearch = () => {
     setValue('')
@@ -28,7 +24,10 @@ const App = () => {
   }
 
   const filterData = (txt) => {
-    const f = data.filter( item => item.username.toUpperCase().includes(txt.toUpperCase()))
+    const f = data.filter( item => (
+                    item.username.toUpperCase().includes(txt.toUpperCase()) ||
+                    item.email.toUpperCase().includes(txt.toUpperCase())
+               ))
     setFiltered(f)
   }
 
